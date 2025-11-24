9d94c51f636ea49061f483afb7d52d76c62c7bd3
Joseph Sak <joseph.sak@gusto.com>
2025-11-23 17:33:22 -0700

    fix: update test-utils with proper type imports and eslint-disable
    - Use type-only imports for TypeScript types
    - Add eslint-disable for react-refresh rule (test utilities, not components)
    - Remove unused initialPath parameter

--- src/test/test-utils.tsx
+++ src/test/test-utils.tsx
@@ -1,5 +1,8 @@
-import { ReactElement } from 'react'
-import { render, RenderOptions } from '@testing-library/react'
+/* eslint-disable react-refresh/only-export-components */
+// Test utilities file - exports test helpers, not components
+import type { ReactElement } from 'react'
+import type { RenderOptions } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import { BrowserRouter, MemoryRouter } from 'react-router-dom'
 import { FavoritesProvider } from '../contexts/FavoritesContext'
 import { SoundCacheProvider } from '../contexts/SoundCacheContext'
@@ -40,15 +43,14 @@ function AllTheProviders({ children, initialEntries }: AllTheProvidersProps) {
 
 interface CustomRenderOptions extends Omit<RenderOptions, 'wrapper'> {
   initialEntries?: string[]
-  initialPath?: string
 }
 
 const customRender = (
   ui: ReactElement,
-  { initialEntries, initialPath, ...renderOptions }: CustomRenderOptions = {}
+  { initialEntries, ...renderOptions }: CustomRenderOptions = {}
 ) => {
   return render(ui, {
-    wrapper: (props) => <AllTheProviders {...props} initialEntries={initialEntries} initialPath={initialPath} />,
+    wrapper: (props) => <AllTheProviders {...props} initialEntries={initialEntries} />,
     ...renderOptions,
   })
 }
